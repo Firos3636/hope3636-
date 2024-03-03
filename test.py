761d393207cba8ee90fc3f8c02561097f8c652da
firos <zorif3636@gmail.comgit config --global user.email zorif3636@gmail.com>
2024-03-03 10:13:06 +0000

    square root

--- test.py
+++ test.py
@@ -1,21 +1,12 @@
-# Python program to check if the number is an Armstrong number or not
+# Find square root of real or complex numbers
+# Importing the complex math module
+import cmath
 
-# take input from the user
-num = int(input("Enter a number: "))
+num = 1+2j
 
-# initialize sum
-sum = 0
+# To take input from the user
+#num = eval(input('Enter a number: '))
 
-# find the sum of the cube of each digit
-temp = num
-while temp > 0:
-   digit = temp % 10
-   sum += digit ** 3
-   temp //= 10
-
-# display the result
-if num == sum:
-   print(num,"is an Armstrong number")
-else:
-   print(num,"is not an Armstrong number")
+num_sqrt = cmath.sqrt(num)
+print('The square root of {0} is {1:0.3f}+{2:0.3f}j'.format(num ,num_sqrt.real,num_sqrt.imag))
 
